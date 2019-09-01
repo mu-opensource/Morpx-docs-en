e8abb7a000a1dbe3d96ab15f724071fab606d339
xuyuping <xuyuping1993@qq.com>
2019-09-01 14:52:17 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -71,7 +71,7 @@ source_suffix = ['.rst', '.md']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -143,7 +143,7 @@ htmlhelp_basename = 'Morpxdoc'
 
 # -- Options for LaTeX pdf output ------------------------------------------------
 
-latex_engine = 'xelatex'
+#latex_engine = 'xelatex'
 
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
